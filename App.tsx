dfdac7c5f29ce6bf0cdd53b1a5beed86b87341f2
meettan <meettan@synergicsoftek.com>
2023-10-17 22:01:56 +0530

    jxdfvbjkdf

---
+++ App.tsx
@@ -0,0 +1,101 @@
+// React Native Bottom Navigation
+// https://aboutreact.com/react-native-bottom-navigation/
+import * as React from 'react';
+
+import
+ MaterialCommunityIcons
+from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+// import ProfileScreen from './pages/ProfileScreen';
+// import SettingsScreen from './pages/SettingsScreen';
+import Home from './pages/Home';
+import DtScreen from './pages/DtScreen';
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function HomeStack() {
+  return (
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{headerShown: false}}
+        >
+        <Stack.Screen
+          name="Home"
+          component={Home} />
+        <Stack.Screen
+          name="DtScreen"
+          component={DtScreen} />
+      </Stack.Navigator>
+  );
+}
+
+function SettingsStack() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Settings"
+      screenOptions={{headerShown: false}}>
+      <Stack.Screen
+        name="Home"
+        component={Home} />
+      <Stack.Screen
+        name="DtScreen"
+        component={DtScreen} />
+    </Stack.Navigator>
+  );
+}
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Feed"
+        screenOptions={({ route }) => ({
+          headerStyle: { backgroundColor: '#42f44b' },
+          headerTintColor: '#fff',
+          headerTitleStyle: { fontWeight: 'bold' },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if (route.name === 'HomeStack') {
+              iconName = focused
+                ? 'home-circle'
+                : 'home-circle-outline';
+            } else if (route.name === 'SettingsStack') {
+              iconName = focused
+                ? 'account-settings'
+                : 'account-settings-outline';
+            }
+            return (
+              <MaterialCommunityIcons
+                name={iconName}
+                size={size}
+                color={color}
+              />
+            );
+          }
+        })}>
+        <Tab.Screen
+          name="HomeStack"
+          component={HomeStack}
+          options={{
+            tabBarLabel: 'Home',
+            title: 'Home',
+          }}  />
+        <Tab.Screen
+          name="SettingsStack"
+          component={SettingsStack}
+          options={{
+            tabBarLabel: 'Settings',
+            title: 'Setting'
+          }} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+export default App;
---
+++ pages/DtScreen.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const DtScreen = () => {
+  return (
+    <View>
+      <Text>DtScreen</Text>
+    </View>
+  )
+}
+
+export default DtScreen
+
+const styles = StyleSheet.create({})
